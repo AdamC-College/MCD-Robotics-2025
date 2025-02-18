38dfe1ead540ee610e33fcba8bf08fe2764103b2
McDowell Robotics Computer <bucholtz@mtsd.org>
2025-02-18 15:26:05 -0500

    updated RobotCommands

--- src/main/java/frc/robot/RobotCommands.java
+++ src/main/java/frc/robot/RobotCommands.java
@@ -1,16 +1,38 @@
 package frc.robot;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkFlexConfigAccessor;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+import com.revrobotics.sim.SparkFlexSim;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel;
+
 import frc.robot.Constants.ClawConstants;
 public class RobotCommands { // Class that holds robot commands involing the claw elevator and sweeper
+    private void Controllers(){
+        XboxController pilot = new XboxController(0);
+        XboxController copilot = new XboxController(1);
+        pilot.getAButtonPressed();
+        
+
+
+    }
     private void claw(){ // Claw Functionality 
-        // Claw Constants 
-        //clawMotor1 - First Motor on Claw
-        //clawMotor2 - Second motor on Claw
-        //kPneumaticsModuleCanId - Pneumatics Setup
-        //kForwardChannel - Pneumatics Forward
-        //kReverseChannel - Pneumatics Reverse
-        //kMaxPressure - Pneumatics Max Pressure
-        //kMinPressure - Pneumatics min Pressure 
-        //kMaxRotationClawSpeed - Motor Max Rotation speed for Claw Motors
+        
+                // Claw Constants 
+                //clawMotor1 - First Motor on Claw
+                //clawMotor2 - Second motor on Claw
+                //kPneumaticsModuleCanId - Pneumatics Setup
+                //kForwardChannel - Pneumatics Forward
+                //kReverseChannel - Pneumatics Reverse
+                //kMaxPressure - Pneumatics Max Pressure
+                //kMinPressure - Pneumatics min Pressure 
+                //kMaxRotationClawSpeed - Motor Max Rotation speed for Claw Motors
+       
+        
+
 
 
 
@@ -23,6 +45,7 @@ public class RobotCommands { // Class that holds robot commands involing the cla
         // orginalPos - starting position for elevator
         // pos1 - base level coral position
         // pos2 - level 2
+        
         // pos3 - level 3
         // pos4 - level 4
     
